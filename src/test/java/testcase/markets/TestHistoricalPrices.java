1717cc51be47f88dbe1f41a74a94ed6827f0133c
pikjian <pikjian@pikjians-MacBook-Air.local>
2022-01-20 00:10:48 +0800

    Create Convert Response message to each field into map

--- src/test/java/testcase/markets/TestHistoricalPrices.java
+++ src/test/java/testcase/markets/TestHistoricalPrices.java
@@ -2,14 +2,11 @@ package testcase.markets;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import testcase.BaseTest;
-
-import java.util.HashMap;
 
 public class TestHistoricalPrices extends BaseTest {
     @Test
@@ -19,27 +16,15 @@ public class TestHistoricalPrices extends BaseTest {
                 "/markets/btc/usd/candles?resolution=15" +
                         "&start_time=1640793880" +
                         "&end_time=1640793885");
+        convertResponseResultToMap(res);
 
-        JsonPath jsonPath = res.jsonPath();
-
-        HashMap<String,String> hm = new HashMap();
-        String jsonPathString = jsonPath.get("result").toString().replace(" ","");
-        String[] s = jsonPathString.substring(2,jsonPathString.length()-2).split(",");
-        for(int i=0;i<s.length;i++){
-            String[] sSplit = s[i].split("=");
-            hm.put(sSplit[0],sSplit[1]);
-        }
-
-//        for (Map.Entry<String, String> set : hm.entrySet()) {
-//            System.out.println(set.getKey() + " = " + set.getValue());
-//        }
-
-        Assert.assertEquals(hm.get("high"),"48012.0","High not match");
-        Assert.assertEquals(hm.get("low"),"47996.0","low not match");
-        Assert.assertEquals(hm.get("time"),"1.6407939E12","time not match");
-        Assert.assertEquals(hm.get("close"),"48012.0","close not match");
-        Assert.assertEquals(hm.get("volume"),"158167.06","volume not match");
-        Assert.assertEquals(hm.get("startTime"),"2021-12-29T16:04:45+00:00","startTime not match");
-        Assert.assertEquals(hm.get("open"),"47997.0","open not match");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(hm.get("high"),"48012.0","High not match");
+        softAssert.assertEquals(hm.get("low"),"47996.0","low not match");
+        softAssert.assertEquals(hm.get("time"),"1.6407939E12","time not match");
+        softAssert.assertEquals(hm.get("close"),"48012.0","close not match");
+        softAssert.assertEquals(hm.get("volume"),"158167.06","volume not match");
+        softAssert.assertEquals(hm.get("open"),"47997.0","open not match");
+        softAssert.assertAll();
     }
 }
